fed6b70a21b02a7e6f11fbe2f563f16f406a7f04
1231245151 <homingi@naver.com>
2026-03-26 14:33:47 +0900

    [0326 홍민기] Heal추가

---
+++ Dice/Heal.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Action.h"
+
+
+class Heal :
+    public Action
+{
+private:
+    int power;
+
+public:
+    virtual void DoAciton(std::vector<Character*>& _self, std::vector<Character*>& _target) override;
+    virtual void PrintInfo() override;
+
+public:
+    Heal(int _power = 1);
+};
+
