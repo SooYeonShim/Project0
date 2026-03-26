e880674919f4da074ee8346d97f577fd0f92b063
JoYoungRae <godyoung36@gmail.com>
2026-03-26 12:04:35 +0900

    [0326 조영래] BattleManager 의사코드 작성

--- Battle/BattleManager.h
+++ Battle/BattleManager.h
@@ -6,19 +6,18 @@
 class BattleManager
 {
 public:
-    static BattleManager& getInstance() 
+    static BattleManager& getInstance()
     {
         static BattleManager instance; // 최초 1회만 생성
         return instance;
-
-        bool Battle(vector<Player>&, int Stage);
-        void AddRerollCount();
     }
 
-private:
-    // 생성자를 private으로 막음
-    BattleManager() {}
+    bool Battle(vector<Player>&, int Stage);
+    void AddRerollCount();    
 
+private:
+
+    BattleManager() {};
     // 복사/대입도 막음
     BattleManager(const BattleManager&) = delete;
     BattleManager& operator=(const BattleManager&) = delete;
