5793b4c9f8bccc0a1f79f311bf29728f55b19857
1231245151 <homingi@naver.com>
2026-03-26 12:48:46 +0900

    [0326 홍민기] Dice클래스 추가 생성.  Action헤더 다이스폴더로 변경

---
+++ Dice/None.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Action.h"
+
+class None :
+	public Action
+{
+public:
+	virtual void DoAciton(std::vector<Character*>& _self, std::vector<Character*>& _target) override {};
+	virtual void PrintInfo() override;
+
+public:
+	None();
+};
+
