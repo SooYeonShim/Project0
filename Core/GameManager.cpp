607f3ad260d3c7be41e972bf46c2b434a35d57e9
langho968 <97336364+langho968@users.noreply.github.com>
2026-03-26 17:01:00 +0900

    [0326 이승재] 1차 게임 흐름 구성

--- Core/GameManager.cpp
+++ Core/GameManager.cpp
@@ -7,7 +7,7 @@ void GameManager::InitializeCharacter(vector<Player>& Players)
     int PlayerCount = 0;
     string NickName;
 
-    for (int i = 0; i < 2; ++i)
+    for (int i = 0; i < 1; ++i)
     {
         int jobChoice;
 
@@ -19,22 +19,54 @@ void GameManager::InitializeCharacter(vector<Player>& Players)
         cin.ignore();
 
         // 입력받은 숫자를 JobType으로 변환하여 추가
-        Players.push_back(Player(NickName, static_cast<JobType>(jobChoice)));
+        Players.push_back(Player(10, Dice(), NickName, static_cast<JobType>(jobChoice)));
     }
 
 }
 
 void GameManager::GameStart()
 {
+
+    BattleManager& BM = BattleManager::getInstance();
+
+    //플레이어 캐릭터 초기화
     InitializeCharacter(Players);
     //초기화 정상작동 확인
     cout << "첫 번째 플레이어 이름: " << Players[0].GetNickname() << " 직업: " << static_cast<int>(Players[0].GetJobType()) << endl;
-    cout << "두 번째 플레이어 이름: " << Players[1].GetNickname() << " 직업: " << static_cast<int>(Players[1].GetJobType()) << endl;
+    //cout << "두 번째 플레이어 이름: " << Players[1].GetNickname() << " 직업: " << static_cast<int>(Players[1].GetJobType()) << endl;
+
+    //처음 스테이지 초기화
+    CurrentStage = 1;
+
+    //스테이지가 3이 되기전까지 전투 반복
+    while (CurrentStage != 3)
+    {
+        //전투 돌입
+        BattleResult(BM.Battle(Players, CurrentStage));
+
+        //게임 종료 후 스테이지 증가 
+        ++CurrentStage;
+
+    }
+
+    //게임 종료
+    return;
 
 }
 
+// 전투 결과에 따라 게임 종료 혹은 경험치&아이템 획득
 void GameManager::BattleResult(bool Result)
 {
+    if (Result == true)
+    {
+        // 경험치 및 아이템 획득
+        return;
+    }
+    else
+    {
+        // 플레이어 캐릭터 전멸 게임 종료
+        return;
+    }
 
 }
 
