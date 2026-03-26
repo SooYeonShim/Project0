be7ba38a837274c8513286c32b37db5dc552b98a
sooyeon <yuiyuij@naver.com>
2026-03-26 12:25:42 +0900

    [0326 심수연] Character 클래스 getter, setter 수정

--- Character/Character.cpp
+++ Character/Character.cpp
@@ -4,3 +4,38 @@
 int Character::GetHP() {
 	return HP;
 }
+
+int Character::GetShield() 
+{
+	return Shield;
+}
+
+Dice Character::GetDice()
+{
+	return CharacterDice;
+}
+
+Action Character::GetCurrentAction()
+{
+	return CurrentAction;
+}
+
+void Character::SetHP(int HP)
+{
+	this->HP = HP;
+}
+
+void Character::SetShield(int Shield)
+{
+	this->Shield = Shield;
+}
+
+void Character::SetDice(Dice CharacterDice)
+{
+	this->CharacterDice = CharacterDice;
+}
+
+Action Character::SetCurrentAction(Action CurrentAction)
+{
+	this->CurrentAction = CurrentAction;
+}
