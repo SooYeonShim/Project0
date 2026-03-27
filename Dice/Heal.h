c121713470c80ba120f651f2abbf54a4ed7c078b
1231245151 <homingi@naver.com>
2026-03-27 10:46:11 +0900

    [0327 홍민기] Action클래스에  DoAction은 대상받기용으로 수정. DoActive추가 이제 여기서 실제로 작업처리함. TemplateManager에 직업다이스 받아오는기능 추가

--- Dice/Heal.h
+++ Dice/Heal.h
@@ -9,8 +9,8 @@ private:
     int power;
 
 public:
-    virtual void DoAciton(std::vector<Character*>& _self, std::vector<Character*>& _target) override;
     virtual void PrintInfo() override;
+    virtual void DoActive() override;
 
     virtual Action* Clone() { return new Heal(*this); }
 
