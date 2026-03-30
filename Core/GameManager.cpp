66b4cbb4d8c5ab53ccd961db2876fd52c9acd4fc
langho968 <97336364+langho968@users.noreply.github.com>
2026-03-30 20:05:40 +0900

    Game manager (#81)
    * [0330 이승재] 경험치 버그 & 직업 별 주사위 확인 수정
    
    * [0330 이승재] 캐릭터 생성 시 힐링포션 추가 및 상점에서 힐링 포션 구매 가능
    
    * [0330 이승재] 플레이어 직업 주사위 출력 수정
    
    * [0330 이승재] 상점에서 플레이어 체력 전체 회복 구현
    
    * [0330 이승재] 플레이어 직업 주사위 표시 수정
    
    * [0330 이승재] 상점 UI 수정 완료
    
    * [0330 이승재] 게임 스토리 및 스테이지 클리어 보상 출력

--- Core/GameManager.cpp
+++ Core/GameManager.cpp
@@ -98,8 +98,13 @@ void GameManager::GameStart()
     UM.ClearMenuBox();
     //처음 스테이지 초기화
     CurrentStage = 1;
+
+    cout << "\"숲 속의 던전은 고요하고, 가끔씩 새소리만 들려옵니다.\"" << endl;
+    cout << "\"던전 안쪽으로 좀 더 들어가자,순찰 중인 듯한 고블린과 마주칩니다.\"" << endl;
+    cout << "\"고블린은 간만에 먹잇감을 발견했다는 듯, 망설임없이 달려듭니다.\"" << endl;
+
     //스테이지가 3이 되기전까지 전투 반복
-    while (CurrentStage != 3)
+    while (CurrentStage != 5)
     {
 
         //전투 돌입
@@ -108,7 +113,8 @@ void GameManager::GameStart()
         //배틀 결과에 따라 전멸 시 즉시 게임 죵로
         if (GameResult == true)
         {
-            //게임 종료 후 스테이지 증가 
+            //게임 종료 후 스테이지 증가
+            PrintStoryClear(CurrentStage);
             ++CurrentStage;
 
         }
@@ -116,36 +122,46 @@ void GameManager::GameStart()
         {
             break;
         }
+        if (CurrentStage == 4)
+        {
+            SM.EnterShop(Players, PlayerMoney);
+        }
 
 
     }
 
-    //상점 진입
-    //SM.EnterShop(Players, PlayerMoney);
-
     //게임 종료
     return;
-
-    //전투 결과에 따라
-    BattleResult(BM.Battle(Players, CurrentStage));
 
 }
 
 // 전투 결과에 따라 게임 종료 혹은 경험치&아이템 획득
 bool GameManager::BattleResult(bool Result)
 {
+    UIManager& UM = UIManager::getInstance();
+
     if (Result == true)
     {
         // 경험치 획득
         for (Player& player : Players)
         {
-            if (player.GetIsDead() != false)
+            if (player.GetIsDead() == false)
             {
                 player.SetExp(player.GetExp() + 10);
+                
             }
         }
         // 돈 획득
         PlayerMoney += 100;
+
+        UM.ClearMainWindowBox();
+        cout << "보상으로 모든 플레이어는 각각 10의 경험치와 100골드를 획득했습니다." << endl;
+        cout << endl;
+        for (Player player : Players)
+        {
+            cout << player.GetName() << "의 경험치: " << player.GetExp() << "/" << player.GetNextLevelExp() << endl;
+        }
+        cout << "현재 보유 골드: " << PlayerMoney << endl;
         return true;
     }
     else
@@ -266,4 +282,39 @@ string GameManager::GetJobName(JobType JobType)
         break;
     }
   }
+
+
+void GameManager::PrintStoryClear(int CurrentStage)
+{
+    switch (CurrentStage)
+    {
+    case 1:
+        cout << endl;
+        cout << "\"더 깊숙이 들어갔지만, 입구에서도 느낀 것처럼 던전은 마물이 산다는 것을 믿기 힘들 정도로 평화로워 보입니다.\"" << endl;
+        cout << "\"하지만 그런 생각도 잠시, 어디선가 소리없이 나타난 늑대와 고블린에게 파티는 포위되었습니다.\"" << endl;
+        break;
+    case 2:
+        cout << endl;
+        cout << "\"주변은 빽빽한 나무들로 인해 상당히 어둡습니다.\"" << endl;
+        cout << "\"뿌리가 우거진 땅을 간신히 넘어서 나아가니, 앞에 희미한 불빛이 보입니다.\"" << endl;
+        cout << "\"당신의 파티는 조심스럽게 다가갑니다.\"" << endl;
+        cout << "\"그것은 오크가 피운 모닥불이었고, 오크 자신의 휴식을 방해한 자들을 용서할 기색이 없습니다.\"" << endl;
+        break;
+    case 3:
+        cout << endl;
+        cout << "\"오크를 쓰러뜨리고 던전의 최심부로 들어가자, 쿵, 하는 위압적인 소리가 들려옵니다.\"" << endl;
+        cout << "\"당신의 파티는 갑자기 들려온 소리에 긴장하며 청각을 곤두세웁니다.\"" << endl;
+        cout << "\"나무 사이로 거대한 그림자가 비치는가 싶더니, 금세 트롤이 모습을 나타냅니다.\"" << endl;
+        cout << "\"트롤은 자신의 몸집에 어울리는 나무 한 그루만한 몽둥이를 들고, 조그만 인간 따위는 파리나 다름없다는 듯 휘둘러대기 시작합니다.\"" << endl;
+        cout << "\"이 트롤이 바로 던전의 주인입니다. 당신의 파티는 전투 준비를 합니다.\"" << endl;
+        break;
+    case 4:
+        cout << endl;
+        cout << "\"생각보다 위압적인 상대는 아니었지만, 운이 따르지 않았다면 죽을 수도 있었습니다.\"" << endl;
+        cout << "\"안도의 한숨을 내쉬며, 당신의 파티는 다친 몸을 이끌고 마을로 돌아갑니다.\"" << endl;
+        break;
+    default:
+        break;
+    }
+}
 
